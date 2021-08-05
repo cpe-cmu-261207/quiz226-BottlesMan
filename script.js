185e136f7d5862de2ee854fb53e8751350941e82
BottlesMan <man2545_@hotmail.com>
2021-08-05 14:51:19 +0700

    Test

--- script.js
+++ script.js
@@ -7,7 +7,21 @@ const color = document.querySelector('#color')
 // define more constants and variables here
 
 btn_toggle.onclick = () => {
-  // your code here
+  // your code here\
+  const toggle = document.getElementById('toggle')
+  if (toggle.innerHTML == "Show Calculation") {
+    let sum = 630610743 + Number(length.value)
+    author.innerHTML = sum
+    toggle.innerHTML = "Show Author"
+  } else if (toggle.innerHTML == "Show Author") {
+    author.innerHTML = "630610743 Nadtaphong Jandaboot"
+    toggle.innerHTML = "Show Calculation"
+  }
 }
 
+if (toggle.innerHTML == "Search") {
+  let UserColor = color(color.value)
+  const Change = document.getElementById('#color')
+  Text.body.style.color = UserColor
+}
 // more codes for Search and Reset buttons here
